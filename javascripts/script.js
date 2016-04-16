b80b62ebe940306f9f627f68938b5e689954e7e3
jaimecastignani <jaimecastignani@gmail.com>
2016-04-16 20:42:55 +0200

    casi finito

--- javascripts/script.js
+++ javascripts/script.js
@@ -29,8 +29,14 @@ abcdario[26] = "Z";
 
 $(document).ready(function () {
     var arrayWords = [];
-    var maxlengthword = 0;
+    var maxlengthWord = 0;
     var arrayAceertadas = [];
+
+    var infoPalabra = {
+        palabra: "",
+        posiciones: [],
+        isAcertada: 0
+    };
 
     $('#add-word').click(function () {
         var newWord = $('#word').val();
@@ -38,12 +44,13 @@ $(document).ready(function () {
         for (var key in sessionStorage) {
             arrayWords = JSON.parse(sessionStorage.getItem("arrayWords"));
         }
-        arrayWords.push(newWord);
-        if (maxlengthword < newWord.length) {
-            maxlengthword = newWord.length;
+        arrayWords.push(newWord.toUpperCase());
+        if (maxlengthWord < newWord.length) {
+            maxlengthWord = newWord.length;
         }
         sessionStorage.setItem("arrayWords", JSON.stringify(arrayWords));
         $('<li>').appendTo('#words').text(newWord);
+        $("#word").text("");
     });
 
     $('#add-play').click(function () {
@@ -53,7 +60,9 @@ $(document).ready(function () {
         var alto = palabras * 2 + 5;
         var ancho = palabras * 2 + 5;
         var content = '<table border=1 id=sletras align= center>';
-
+        /*
+         * llenamos la tabla de letras aleatorias
+         */
         for (x = 0; x < alto; x++) {
             content += '<tr>';
 
@@ -70,18 +79,55 @@ $(document).ready(function () {
 
 
         var arrayNumbersAleatori = [];
+        /*
+         * Pintamos las plabras en la tabla de forma aleatoria pero con cierto control para que no se sobreescriban
+         */
 
         for (r = 0; r < arrayWords.length; r++) {
-            var aleatoriPosition = Math.floor(Math.random() * (alto - 0)) + 0;
+            var pWord = "";
+            var posiWords = [];
+            //primera palabra
+            if (r == 0) {
+                for (f = 0; f < arrayWords[r].length; f++) {
+                    var xa = f + 1 + "" + ancho - 1;
+                    $("#" + xa).text(arrayWords[r].charAt(f));
+                    pWord += arrayWords[r].charAt(f);
+                    infoPalabra.posiciones.push("#" + xa);
+                }
+                infoPalabra.palabra = pWord;
+                sessionStorage.setItem(pWord, JSON.stringify(infoPalabra));
+                infoPalabra.posiciones = [];
+            } else if (r == 1) {
+                //segunda palabra
+                for (f = 0; f < arrayWords[r].length; f++) {
+                    var xa = f + 3 + "" + ancho - 3;
+                    $("#" + xa).text(arrayWords[r].charAt(f));
+                    pWord += arrayWords[r].charAt(f);
+                    infoPalabra.posiciones.push("#" + xa);
+                }
+                infoPalabra.palabra = pWord;
+                sessionStorage.setItem(pWord, JSON.stringify(infoPalabra));
+                infoPalabra.posiciones = [];
+            } else {
+                //El resto de palabras horizontales pero de forma aleatoria
+                var aleatoriPosition = Math.floor(Math.random() * (alto - 0)) + 0;
 
-            while ($.inArray(aleatoriPosition, arrayNumbersAleatori) !== -1) {
-                aleatoriPosition = Math.floor(Math.random() * (alto - 0)) + 0;
-            }
-            arrayNumbersAleatori.push(aleatoriPosition);
+                // comrpobamos que la posicion donde vamos  a escribir no se haya utilzado para no sobrescribir
+                while ($.inArray(aleatoriPosition, arrayNumbersAleatori) !== -1) {
+                    aleatoriPosition = Math.floor(Math.random() * (alto - 0)) + 0;
+                }
 
-            for (z = 0; z < arrayWords[r].length; z++) {
-                $("#" + aleatoriPosition + z).text(arrayWords[r].charAt(z));
-
+                arrayNumbersAleatori.push(aleatoriPosition);
+                var other = Math.floor(Math.random() * (4 - 0)) + 0;
+                for (z = 0; z < arrayWords[r].length; z++) {
+                    var xo = aleatoriPosition + "" + (z + other);
+                    $("#" + xo).text(arrayWords[r].charAt(z));
+                    pWord += arrayWords[r].charAt(z);
+                    infoPalabra.posiciones.push("#" + xo);
+                }
+                infoPalabra.palabra = pWord;
+                sessionStorage.setItem(pWord, JSON.stringify(infoPalabra));
+                infoPalabra.posiciones = [];
             }
         }
 
@@ -89,13 +135,29 @@ $(document).ready(function () {
             $(this).css('backgroundColor', '#ff0000');
             var leter = $(this).html();
             selectedWord += leter;
-            if (selectedWord.length > maxlengthword) {
+            if (selectedWord.length > maxlengthWord) {
                 selectedWord = '';
                 $('td').css('backgroundColor', '#333333');
+                palabrasAcertadas();
             } else {
                 for (pos = 0; pos < arrayWords.length; pos++) {
-                    if (selectedWord.toUpperCase() === arrayWords[pos].toUpperCase()) {
-                        $(this).css('backgroundColor', '#33cc33');
+                    if (selectedWord == arrayWords[pos]) {
+                        infoPalabra = JSON.parse(sessionStorage.getItem(arrayWords[pos]));
+                        infoPalabra.isAcertada = 1;
+                        sessionStorage.setItem(arrayWords[pos], JSON.stringify(infoPalabra));
+                        $('td').css('backgroundColor', '#333333');
+                        palabrasAcertadas();
+                    }
+                }
+            }
+
+            function palabrasAcertadas() {
+                for (var key in sessionStorage) {
+                    var infoPalabra = JSON.parse(sessionStorage.getItem(key));
+                    if (infoPalabra.isAcertada == 1) {
+                        for (d = 0; d < infoPalabra.posiciones.length; d++) {
+                            $(infoPalabra.posiciones[d]).css('backgroundColor', '#33cc33');
+                        }
                     }
                 }
             }
